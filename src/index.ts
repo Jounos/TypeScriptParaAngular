19903adcb439bf2bbf3e3156f9eb79466e5a2b02
Geovanny Neves Liberato <geovanny.liberato@inventsoftware.com.br>
2025-10-08 08:43:14 -0300

    Criado tsconfig.json para facilitar a compilação dos arquivos

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+type Hero = {
+    name: string;
+    vulgo: string;
+}
+
+function printaObjeto(pessoa: Hero) {
+
+    console.log(pessoa);
+}
+
+printaObjeto({
+    name: 'Batman',
+    vulgo: 'Bruce Wayne'
+})
