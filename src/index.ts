7acb97c83e544567a05db37a5ea4162477ee31ed
Geovanny Neves Liberato <geovanny.liberato@inventsoftware.com.br>
2025-10-08 09:00:52 -0300

    Apresentado os tipos de variável do typescript

--- src/index.ts
+++ src/index.ts
@@ -12,3 +12,21 @@ printaObjeto({
     name: 'Batman',
     vulgo: 'Bruce Wayne'
 })
+
+/**
+ * Variáveis
+ */
+
+// Tipos primitivos: boolean, number, string
+    let ligado: boolean = false;
+    let nome: string = "Geovanny";
+    let altura: number = 1.8;
+    let idade: number = 28;
+
+// Tipos especiais: null, undefined
+let nulo: null = null;
+let indefinido: undefined = undefined;
+
+// Tipos abrangentes: any, void
+let retorno: void;
+let retornoView: any = false;
